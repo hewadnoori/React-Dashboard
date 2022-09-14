4c1f56aac25a18e1ebbbfeaea96f0995d02d0713
Hewad Noori <hewadnoori16@gmail.com>
2022-09-13 21:43:22 -0400

    done with setup

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,4 +1,7 @@
-
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {FiSettings} from 'react-icons/fi'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 function App() {
@@ -6,7 +9,18 @@ function App() {
     <div className="App">
        <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+      </h1>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex:'1000'}}>
+            <TooltipComponent content="Settings" position='Top'>
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
