13609342ca2873b3acac5aae65b2be7a88aa6e90
Hewad Noori <hewadnoori16@gmail.com>
2022-09-14 19:38:38 -0400

    finished sidebar

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,8 +6,10 @@ import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line} from './pages'
 import './App.css';
 
+import {useStateContext} from './contexts/ContextProvider'
+
 function App() {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
   return (
     <div className="App">
       <BrowserRouter>
